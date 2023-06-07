18e38a62bdc07217882bf2fce73ada7eb34711d5
riandroraul <riandroraul@gmail.com>
2023-06-07 22:07:50 +0700

    Update typeorm

---
+++ src/books/entity/book.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Book extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+
+  @Column()
+  category: string;
+
+  @Column()
+  year: number;
+}
